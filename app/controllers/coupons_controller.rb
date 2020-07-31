8cdb631bf8e5f81e795cc4ba340c49e814ee0d5f
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-31 15:25:14 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,20 @@
+class CouponsController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+  end
+  
+end
