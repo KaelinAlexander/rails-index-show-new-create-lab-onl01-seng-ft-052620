ba58b9ab047742166ed0c92d4897352faff0bfed
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-31 15:39:22 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,6 +12,7 @@ class CouponsController < ActionController::Base
   end
 
   def create
+    byebug
     @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
     redirect_to coupon_path(@coupon)
   end
