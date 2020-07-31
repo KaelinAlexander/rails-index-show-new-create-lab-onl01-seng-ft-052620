607369ca415535f948efaf47029c078ae677257a
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-31 15:17:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -1,1 +1,12 @@
-class CreateCoupons < ActiveRecord::Base
+class CreateCoupons < ActiveRecord::Migration
+
+  def change
+    create_table :coupons do |t|
+      t.string : , null: false
+      t.string : , null: false
+
+      t.timestamps null: false
+    end
+  end 
+
+end 
